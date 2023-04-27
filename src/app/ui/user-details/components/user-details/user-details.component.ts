3bfba79ed91d2a8d87946d99544dbc76cdc17e97
mateusz.dobrowolski@valueadd.pl <mateusz.dobrowolski@valueadd.pl>
2023-04-27 21:35:24 +0200

    add new input and storybook control

--- src/app/ui/user-details/components/user-details/user-details.component.ts
+++ src/app/ui/user-details/components/user-details/user-details.component.ts
@@ -8,4 +8,5 @@ import { UiUserDetails } from "./user-details.interface";
 })
 export class UserDetailsComponent {
   @Input() user?: UiUserDetails;
+  @Input() notificationCount = 0;
 }
--- src/app/ui/user-details/components/user-details/user-details.stories.ts
+++ src/app/ui/user-details/components/user-details/user-details.stories.ts
@@ -1,15 +1,39 @@
 import type { Meta, StoryObj } from "@storybook/angular";
 import { UserDetailsComponent } from "./user-details.component";
+import { UiUserDetails } from "./user-details.interface";
 
+// story meta config
 const meta: Meta<UserDetailsComponent> = {
   title: "User/UserDetails",
-  component: UserDetailsComponent
+  component: UserDetailsComponent,
+  argTypes: {
+    notificationCount: {
+      options: [0, 1, 9, 15, 99, 123, 999, 2317],
+      defaultValue: 0,
+      control: { type: "radio" }
+    }
+  }
 };
-
 export default meta;
 
+// mocks
+const userMock: UiUserDetails = {
+  firstName: "John",
+  lastName: "Smith",
+  email: "john.smith@abc.efd",
+  avatar: {
+    url: "https://placehold.it/100x100",
+    alt: "John Smith avatar"
+  }
+};
+
+// stories
 type UserDetailsStory = StoryObj<UserDetailsComponent>;
 
-export const primary: UserDetailsStory = {};
+export const primary: UserDetailsStory = {
+  args: {
+    user: userMock
+  }
+};
 
 
